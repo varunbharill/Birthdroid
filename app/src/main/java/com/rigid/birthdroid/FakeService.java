8acf91a641fc377c8a4542f7e07fadf87ae0e65e
Varun Bharill <varunbharill@dhcp-v098-008.mobile.uci.edu>
2016-11-10 14:33:39 -0800

    Registering a hijack service

---
+++ app/src/main/java/com/rigid/birthdroid/FakeService.java
@@ -0,0 +1,86 @@
+package com.rigid.birthdroid;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class FakeService extends IntentService {
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_FOO = "com.rigid.birthdroid.action.FOO";
+    private static final String ACTION_BAZ = "com.rigid.birthdroid.action.BAZ";
+
+    // TODO: Rename parameters
+    private static final String EXTRA_PARAM1 = "com.rigid.birthdroid.extra.PARAM1";
+    private static final String EXTRA_PARAM2 = "com.rigid.birthdroid.extra.PARAM2";
+
+    public FakeService() {
+        super("FakeService");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionFoo(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, FakeService.class);
+        intent.setAction(ACTION_FOO);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionBaz(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, FakeService.class);
+        intent.setAction(ACTION_BAZ);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            long smsId = intent.getExtras().getLong("datetimeCreated");
+            Log.v("vbharill", "smsid" + smsId + "callno" + intent.getExtras().getInt("callno"));
+            Log.v("vbharill", intent.getExtras().getString("attackmsg"));
+            Log.v("vbharill", "legitimate msg recieved by a hijacker service");
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionFoo(String param1, String param2) {
+        // TODO: Handle action Foo
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionBaz(String param1, String param2) {
+        // TODO: Handle action Baz
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
